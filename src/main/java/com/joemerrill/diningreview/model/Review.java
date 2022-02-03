5d4e722e0bf7e71069ef201f89ab591a3acd7890
Joe Merrill <joesmerrill@icloud.com>
2022-02-03 14:25:56 -0600

    Remove @Column annotations and utilize default JPA/Hibernate database naming conventions for Review class/entity.

--- src/main/java/com/joemerrill/diningreview/model/Review.java
+++ src/main/java/com/joemerrill/diningreview/model/Review.java
@@ -9,34 +9,19 @@ import javax.persistence.*;
  * Represents a Dining Review
  */
 @Entity
-@Table(name = "REVIEWS")
+@Getter
+@Setter
 public class Review {
 
     @Id
     @GeneratedValue
     private Long id;
 
-    @Column(name = "SUBMITTED_BY")
-    @Getter @Setter
-    private String submittedBy;
-
-    @Column(name = "RESTAURANT_ID")
-    @Getter @Setter
     private Long restaurantId;
-
-    @Column(name = "PEANUT_SCORE")
-    @Getter @Setter
+    private String submittedBy;
+    private String review;
     private Integer peanutScore;
-
-    @Column(name = "EGG_SCORE")
-    @Getter @Setter
     private Integer eggScore;
-
-    @Column(name = "DAIRY_SCORE")
-    @Getter @Setter
     private Integer dairyScore;
-
-    @Column(name = "COMMENT")
-    @Getter @Setter
-    private String comment;
+    private Integer glutenScore;
 }
