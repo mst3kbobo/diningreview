2fcf6bdbc08192fbda33b30f66bee32ae0533a76
Joe Merrill <joesmerrill@icloud.com>
2022-02-04 16:17:12 -0600

    RestaurantController GET Mapping for search by Zip Code and interest/allergy.

--- src/main/java/com/joemerrill/diningreview/controller/RestaurantController.java
+++ src/main/java/com/joemerrill/diningreview/controller/RestaurantController.java
@@ -2,10 +2,10 @@ package com.joemerrill.diningreview.controller;
 
 import com.joemerrill.diningreview.model.Restaurant;
 import com.joemerrill.diningreview.repository.RestaurantRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
 
@@ -27,5 +27,57 @@ public class RestaurantController {
     @GetMapping("/{id}")
     public Optional<Restaurant> getRestaurantById(@PathVariable Long id) {
         return restaurantRepository.findById(id);
+    }
+
+    @GetMapping("/search")
+    public Iterable<Restaurant> searchRestaurants(
+            @RequestParam(name = "zipcode") String zipCode,
+            @RequestParam String interest
+    ) {
+
+        // TODO: Validate Zip Code
+        Integer zip = Integer.parseInt(zipCode);
+
+        Iterable<Restaurant> restaurants;
+
+        switch (interest.toLowerCase()) {
+            case "peanut":
+                restaurants = restaurantRepository.findByZipCodeAndPeanutScoreNotNullOrderByPeanutScoreDesc(zip);
+                break;
+            case "egg":
+                restaurants = restaurantRepository.findByZipCodeAndEggScoreNotNullOrderByEggScoreDesc(zip);
+                break;
+            case "dairy":
+                restaurants = restaurantRepository.findByZipCodeAndDairyScoreNotNullOrderByDairyScoreDesc(zip);
+                break;
+            case "gluten":
+                restaurants = restaurantRepository.findByZipCodeAndGlutenScoreNotNullOrderByGlutenScoreDesc(zip);
+                break;
+            default:
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        return restaurants;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Restaurant addRestaurant(@RequestBody Restaurant restaurant) {
+
+        // Restaurants with same name and zip code should not exist
+        if (ObjectUtils.isEmpty(restaurant.getName()) || ObjectUtils.isEmpty(restaurant.getZipCode())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        Optional<Restaurant> existingRestaurantOptional =
+                restaurantRepository.findByNameAndZipCode(restaurant.getName(), restaurant.getZipCode());
+
+        if (existingRestaurantOptional.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT);
+        }
+
+        // TODO: Validate Zip Code
+
+        return restaurantRepository.save(restaurant);
     }
 }
